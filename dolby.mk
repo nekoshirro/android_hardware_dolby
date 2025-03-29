ed7718f82c557549daa3cfcf765c09c53e0391ec
New Author Name <fabian.leutenegger@bluewin.ch>
2025-03-30 02:00:08 +0530

    dolby: Link dolby blobs against v33 libstagefright_foundation
    * fixes crashes and absurdly high CPU usage while using dolby
    
    Co-authored-by: Woomymy <woomy@woomy.be>
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- dolby.mk
+++ dolby.mk
@@ -38,9 +38,8 @@ PRODUCT_COPY_FILES += \
     $(DOLBY_PATH)/configs/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
 
 # Dolby VNDK libs
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v33/arm/arch-arm-armv7-a-neon/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefright_foundation-v33.so \
-    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33.so
+PRODUCT_PACKAGES += \
+    libstagefright_foundation-v33
 
 # Overlays    
 PRODUCT_PACKAGES += \
