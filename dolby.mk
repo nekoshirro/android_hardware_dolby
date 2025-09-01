268b76fb7245b8174da79a9b5f476bbf0305d8e8
beingashwani <ashwanic177@gmail.com>
2025-09-01 17:25:19 +0000

    Revert "dolby: Implement DSPVolumeSynchronizer"
    This reverts commit ba7aa808a50e42eb17163dfa92a4d1e65644423b.
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- dolby.mk
+++ dolby.mk
@@ -99,10 +99,6 @@ PRODUCT_VENDOR_PROPERTIES += \
 PRODUCT_PACKAGES += \
     RemovePackagesDolby
 
-# DSP Volume Synchronizer
-PRODUCT_PACKAGES += \
-    DSPVolumeSynchronizer
-
 # XiaomiDolby
 PRODUCT_PACKAGES += \
     XiaomiDolby
