cba7913e1b772ae76c54d743a0b13a92e3384ac0
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-01-22 07:47:25 +0530

    dolby: Move manifest to blueprint Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- dolby.mk
+++ dolby.mk
@@ -29,8 +29,10 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DOLBY_PATH)/sepolicy/vendor
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DOLBY_PATH)/dolby_framework_matrix.xml
-DEVICE_MANIFEST_FILE += $(DOLBY_PATH)/vendor.dolby.hardware.dms@2.0-service.xml
-DEVICE_MANIFEST_FILE += $(DOLBY_PATH)/vendor.dolby.media.c2.xml
+
+PRODUCT_PACKAGES += \
+    vendor.dolby.hardware.dms@2.0-service.xml \
+    vendor.dolby.media.c2.xml
     
 # Configs
 PRODUCT_COPY_FILES += \
