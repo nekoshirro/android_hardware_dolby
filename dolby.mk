438e05b3cfbc37d9e99626f8f440b8f99b21eb78
Hafidz Muzakky <ais.muzakky@gmail.com>
2026-09-19 13:51:27 +0000

    dolby: drop vendor.dolby.media.c2.xml
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- dolby.mk
+++ dolby.mk
@@ -31,9 +31,8 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DOLBY_PATH)/sepolicy/vendor
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DOLBY_PATH)/dolby_framework_matrix.xml
 
 PRODUCT_PACKAGES += \
-    vendor.dolby.hardware.dms@2.0-service.xml \
-    vendor.dolby.media.c2.xml
-    
+    vendor.dolby.hardware.dms@2.0-service.xml
+
 # Configs
 PRODUCT_COPY_FILES += \
     $(DOLBY_PATH)/proprietary/vendor/etc/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
